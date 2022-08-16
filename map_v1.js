cdfd199f7e5c8f7336e9e8571e345009c3f677de
mahmood <mahmood@DESKTOP-H79MVT9>
2022-08-16 21:43:47 +0430

    loadmap and mapclick and measerment

---
+++ map_v1.js
@@ -0,0 +1,120 @@
+let map;
+let layer;
+let mapView;
+let measurement;
+let publicDivMap
+
+
+function loadMap(url, divMap, fn){
+    require(["esri/config", "esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer", "esri/layers/MapImageLayer",
+            "esri/widgets/Measurement"],
+        function (config, Map, MapView, FeatureLayer, MapImageLayer, Measurement) {
+
+            publicDivMap = divMap;
+            
+            layer = new MapImageLayer({
+                //url: "https://gisfava.isfahan.ir/arcgis80/rest/services/BaseMap/GISBaseMap1/MapServer"
+                url : url
+                //url : "http://gisfava.isfahan.ir:6080/arcgis/rest/services/BaseMap/GISBaseMap/MapServer"
+            });
+
+            map = new Map({
+            //basemap: "osm-light-gray",
+            });
+
+            mapView = new MapView({
+                map: map,
+                //center: [-107.71808390060319, 42.68918060911958],
+                //center: [51.6573571480861, 32.64777704555835],
+                //zoom: 20,
+                container: divMap
+            });
+
+            //map.layers.addMany([layer]);
+            map.add(layer);
+
+            measurement = new Measurement();
+
+        
+
+            fn();
+    })
+}
+
+
+function mapClick(fn){
+    mapView.on("click", function(e){
+        //alert("x:" + e.mapPoint.x + " y:" + e.mapPoint.y );
+
+        fn("x:" + e.mapPoint.x + " y:" + e.mapPoint.y);
+    });
+}
+
+function measurementTools(){
+    let toolbarStr = '<div id="toolbarDiv" class="esri-component esri-widget">'+
+    '<button'+
+      'id="distance"'+
+      'class="esri-widget--button esri-interactive esri-icon-measure-line"'+
+      'title="Distance Measurement Tool"'+
+    '></button>'+
+    '<button'+
+      'id="area"'+
+      'class="esri-widget--button esri-interactive esri-icon-measure-area"'+
+      'title="Area Measurement Tool"'+
+    '></button>'+
+    '<button'+
+      'id="clear"'+
+      'class="esri-widget--button esri-interactive esri-icon-trash"'+
+      'title="Clear Measurements"'+
+    '></button>'+
+  '</div>';
+
+  document.getElementsByTagName('body').innerHTML += toolbarStr;
+    
+    let activeView = mapView;
+
+    const distanceButton = document.getElementById("distance");
+    const areaButton = document.getElementById("area");
+    const clearButton = document.getElementById("clear");
+
+    distanceButton.addEventListener("click", () => {
+        distanceMeasurement();
+    });
+    areaButton.addEventListener("click", () => {
+        areaMeasurement();
+    });
+    clearButton.addEventListener("click", () => {
+        clearMeasurements();
+    });
+
+    activeView.set({
+        container: publicDivMap
+    });
+    // Add the appropriate measurement UI to the bottom-right when activated
+    activeView.ui.add(measurement, "bottom-right");
+    // Set the views for the widgets
+    measurement.view = activeView;
+
+    // Call the appropriate DistanceMeasurement2D or DirectLineMeasurement3D
+    function distanceMeasurement() {
+        const type = activeView.type;
+        measurement.activeTool =
+            type.toUpperCase() === "2D" ? "distance" : "direct-line";
+        distanceButton.classList.add("active");
+        areaButton.classList.remove("active");
+    }
+  
+    // Call the appropriate AreaMeasurement2D or AreaMeasurement3D
+    function areaMeasurement() {
+        measurement.activeTool = "area";
+        distanceButton.classList.remove("active");
+        areaButton.classList.add("active");
+    }
+
+    // Clears all measurements
+    function clearMeasurements() {
+        distanceButton.classList.remove("active");
+        areaButton.classList.remove("active");
+        measurement.clear();
+    }
+}
